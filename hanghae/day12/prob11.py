5134ea6dc3439bcae6961ffb4f2a587fc86a2250
DooDuZ <sin9158@naver.com>
2024-06-13 21:38:18 +0900

    day12 완료

--- hanghae/day12/prob11.py
+++ hanghae/day12/prob11.py
@@ -1,19 +1,126 @@
+# BOJ 20056 마법사 상어와 파이어볼
+
 import sys
+from collections import defaultdict
 
 input = sys.stdin.readline
+N, M, K = list(map(int, input().split()))
+dr = [-1, -1, 0, 1, 1, 1, 0, -1]
+dc = [0, 1, 1, 1, 0, -1, -1, -1]
 
 
 def get_input():
-    params = []
+    fireballs = []
 
-    return params
+    for _ in range(M):
+        fireballs.append(list(map(int, input().split())))
+
+    return fireballs
 
 
-def solution(params):
-    pass
+# 속력만큼 움직인 후 mod
+def move_fireball(fireball):
+    row, col, m, s, d, cnt = fireball
+
+    row += dr[d] * s
+    row %= N
+    col += dc[d] * s
+    col %= N
+
+    fireball[0] = row
+    fireball[1] = col
+
+
+def merge_fireball(f_list):
+    merged = [f_list[0][0], f_list[0][1], 0, 0, 0, 0]
+
+    is_even = True
+
+    start = f_list[0][4] % 2
+
+    for fireball in f_list:
+        r, c, m, s, d, cnt = fireball
+        merged[2] += m
+        merged[3] += s
+        merged[5] += 1
+
+        if fireball[4] % 2 != start:
+            is_even = False
+
+    if is_even:
+        merged[4] = 0
+    else:
+        merged[4] = 1
+
+    return merged
+
+
+def divide_fireball(fireball):
+    ret = []
+
+    r, c, m, s, d, cnt = fireball
+
+    m //= 5
+    s //= cnt
+
+    if m == 0:
+        return ret
+
+    start = 0
+    if d == 1:
+        start = 1
+
+    for i in range(start, 8, 2):
+        ret.append([r, c, m, s, i, 1])
+
+    return ret
+
+
+def solution(fireballs):
+
+    # 좌표를 키로 사용하는 fireball 저장소
+    info = defaultdict(list)
+
+    for fireball in fireballs:
+        # 몇 개가 merge되었는지 cnt 정보 추가
+        fireball.append(1)
+        row, col = fireball[0], fireball[1]
+        info[(row, col)].append(fireball)
+
+    for _ in range(K):
+        # 이동/머지/분할 후의 정보 저장용
+        n_info = defaultdict(list)
+
+        # 각 좌표의 파이어볼을 이동시킨 후 저장
+        for f_list in info.values():
+            for fireball in f_list:
+                move_fireball(fireball)
+                n_info[(fireball[0], fireball[1])].append(fireball)
+
+        # 이동한 파이어볼들 조작
+        for key in n_info.keys():
+            # 좌표에 파이어볼이 하나만 있다면 pass
+            if len(n_info[key]) < 2:
+                continue
+            f_list = n_info[key]
+            # 겹친 파이어볼 병합
+            fireball = merge_fireball(f_list)
+            # 합친 파이어볼 나누기
+            n_info[key] = divide_fireball(fireball)
+
+            if not f_list:
+                del n_info[key]
+
+        info = n_info
+
+    total = 0
+
+    for f_list in info.values():
+        for fireball in f_list:
+            total += fireball[2]
+
+    return total
 
 
 if __name__ == "__main__":
-    params = get_input()
-    for param in params:
-        print(solution(param))
+    print(solution(get_input()))
--- hanghae/day12/prob12.py
+++ hanghae/day12/prob12.py
@@ -1,19 +1,172 @@
+# BOJ 21609 상어 중학교
+
 import sys
+from collections import deque
+from heapq import *
 
 input = sys.stdin.readline
+N, M = list(map(int, input().split()))
+dr = [1, -1, 0, 0]
+dc = [0, 0, 1, -1]
 
 
 def get_input():
-    params = []
+    cells = []
 
-    return params
+    for _ in range(N):
+        cells.append(list(map(int, input().split())))
+
+    return cells
 
 
-def solution(params):
-    pass
+def check_range(row, col):
+    return 0 <= row < N and 0 <= col < N
+
+
+def get_cnt(q, visited, color, cells):
+    # 무지개 블록 개수 세기
+    rainbow = 0
+    # 넓이
+    cnt = 0
+    while q:
+        row, col = q.popleft()
+        cnt += 1
+
+        if cells[row][col] == 0:
+            rainbow += 1
+
+        for i in range(4):
+            nr = row + dr[i]
+            nc = col + dc[i]
+
+            if (
+                check_range(nr, nc)
+                and visited[nr][nc]
+                and (cells[nr][nc] == color or cells[nr][nc] == 0)
+            ):
+                visited[nr][nc] = False
+                q.append([nr, nc])
+
+    # 맥스힙 사용 예정이므로 -
+    return [-cnt, -rainbow]
+
+
+def rotate(cells):
+    new_cells = []
+
+    for i in range(N - 1, -1, -1):
+        row = []
+        for j in range(N):
+            row.append(cells[j][i])
+        new_cells.append(row)
+
+    return new_cells
+
+
+def drop(cells):
+    # 아래부터 내려줘야함
+    for i in range(N - 1, -1, -1):
+        for j in range(N):
+            if cells[i][j] >= 0:
+                nr = i
+                # -2 == 공백값으로 사용
+                while check_range(nr + 1, j) and cells[nr + 1][j] == -2:
+                    nr += 1
+                # 값이 변한 경우에만 내려주기
+                if nr != i:
+                    cells[nr][j] = cells[i][j]
+                    cells[i][j] = -2
+
+
+def uncheck(visited, rainbow_set):
+    for coord in rainbow_set:
+        r, c = coord
+        visited[r][c] = True
+
+
+def remove_blocks(row, col, cells):
+    color = cells[row][col]
+
+    q = deque()
+    visited = [[True for _ in range(N)] for _ in range(N)]
+    visited[row][col] = False
+    q.append([row, col])
+
+    while q:
+        r, c = q.popleft()
+
+        # 공백 처리
+        cells[r][c] = -2
+
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if (
+                check_range(nr, nc)
+                and visited[nr][nc]
+                and (cells[nr][nc] == color or cells[nr][nc] == 0)
+            ):
+                visited[nr][nc] = False
+                q.append([nr, nc])
+
+
+def store_rainbow(rainbow_set, cells):
+    for i, row in enumerate(cells):
+        for j, cell in enumerate(row):
+            if cell == 0:
+                rainbow_set.add((i, j))
+
+
+def solution(cells):
+    score = 0
+    # 무지개블럭은 여러 일반 블록이 공유할 수 있으므로, 새로운 탐색마다 방문 체크를 해제해줘야함
+    # 빠른 체크해제를 위한 좌표 저장 set
+    rainbow_set = set()
+
+    while True:
+        # 무지개 블럭 위치 저장
+        store_rainbow(rainbow_set, cells)
+        heap = []
+        visited = [[True for _ in range(N)] for _ in range(N)]
+        q = deque()
+
+        for i in range(N):
+            for j in range(N):
+                if visited[i][j] and cells[i][j] > 0:
+                    visited[i][j] = False
+                    # 무지개 블록은 다른 그룹도 사용할 수 있으므로 체크 해제
+                    uncheck(visited, rainbow_set)
+                    q.append([i, j])
+                    # 그룹의 넓이와 rainbow 블록 개수
+                    ret = get_cnt(q, visited, cells[i][j], cells)
+                    # 오름차순이므로 -
+                    ret.append(-i)
+                    ret.append(-j)
+
+                    heappush(heap, ret)
+
+        # 지운 블럭 없거나 최대 그룹 크기가 1이면
+        if not heap or heap[0][0] == -1:
+            break
+
+        # 탐색 끝났으면 꺼내주고
+        area, rainbow_cnt, r, c = heappop(heap)
+
+        # 점수 더해주고
+        score += area**2
+        # 큰 그룹 삭제하고
+        remove_blocks(-r, -c, cells)
+        # 중력 작용 하고
+        drop(cells)
+        # 돌리고
+        cells = rotate(cells)
+        # 다시 중력 작용
+        drop(cells)
+        # 블럭들 위치가 바뀌었으니 초기화
+        rainbow_set.clear()
+
+    return score
 
 
 if __name__ == "__main__":
-    params = get_input()
-    for param in params:
-        print(solution(param))
+    print(solution(get_input()))
